46cfa6e5820bc0b4871026c0235692635d19b62c
rezanur <rezanur@uii.ac.id>
2018-04-27 16:00:59 +0700

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Google Maps Apps With Angular 5';
+  latitude  = -7.687675;
+  longitude = 110.412922;
+
+  onChooseLocation(event) {
+    this.latitude = event.coords.lat;
+    this.longitude = event.coords.lng;
+    console.log(event);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, ApplicationRef } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
+
+import { AgmCoreModule } from '@agm/core';
 
 
 @NgModule({
@@ -10,7 +14,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AgmCoreModule.forRoot({
+      apiKey: 'AIzaSyDnBM9w4D4M2VzUoX82k9f1H2ojC1l_u-U'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
